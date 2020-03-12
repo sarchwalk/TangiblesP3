1dad5420c6f39d63223897a52a882c0ac5924834
Gabelken <gkenna899@gmail.com>
2020-03-11 22:47:57 -0600

    Add comments to arduino I/O code

--- Arduino/RecieveData/Branch.ino
+++ Arduino/RecieveData/Branch.ino
@@ -1,35 +1,66 @@
-int growthLevels[] = { 100, 100, 100 };
-
-int expectedTalkTime[] = { 300, 50, 60 };
-
-int growthMin = 0;
-int growthMax = 255;
-
-//
+/**
+ * Code to initialize branch hardware.
+ */
 void setupBranches() {
-  for(int b = 0; b < NUM_BRANCHES; b++) {
-    pinMode(ledMains[b], OUTPUT);
-    analogWrite(ledMains[b], growthLevels[b]);
+  //TODO: Replace with hardware setup
+  for(int branch = 0; branch < NUM_BRANCHES; branch++) {
+    setupFakeBranch(branch);
   }
 }
 
+/**
+ * TODO
+ */
+void grow(int branch, int amount) {
+  //TODO: replace with hardware code
+  fakeGrow(branch, amount);
+}
+
+/**
+ * Determines how much growth a branch should have based on monthly calling amounts.
+ * 
+ *  branch    {int} The branch needing a growth rate
+ *  talkTime  {int} How much time was spent talking to that branch's group in a month
+ *  
+ * Returns:
+ *  1 for poor growth
+ *  2 for average growth
+ *  3 for great growth
+ */
 int calcGrowth(int branch, int talkTime) {
+
+  // Get the threshold using the expected average talk time value for the branch
   int threshold = expectedTalkTime[branch];
-  
-  if (talkTime < (threshold - (threshold/2))) {
+
+  /**
+   * Split categories based on proximity to threshold
+   *    Bad - less than 50% expected talk time
+   *    Average - between 50% and 150% expected talk time
+   *    Amazing - greater than 150% expected talk time
+   */
+  if (talkTime < (threshold*0.5) {
     return 1; // Bad
   }
-  if(talkTime < (threshold + (threshold/2))) {
+  if(talkTime < (threshold*1.5) {
     return 2; // Average
   }
   return 3; // Amazing!
 }
 
-void grow(int branch, int amount) {
-  growthLevels[branch] = (amount * 100);
-  
-  if(growthLevels[branch] < growthMin) { growthLevels[branch] = growthMin; }
-  else if(growthLevels[branch] > growthMax) { growthLevels[branch] = growthMax; }
-  
-  analogWrite(ledMains[branch], growthLevels[branch]);
+/**
+ * TO BE REMOVED
+ *  - Functions and hardware details for mocking data interaction via leds -
+ */
+
+ 
+int ledMains[NUM_BRANCHES] = { 11, 10, 9 };
+
+void setupFakeBranch(int branch) {
+  pinMode(ledMains[branch], OUTPUT);
+  analogWrite(ledMains[branch], 100);
+}
+
+void fakeGrow(int branch, int amount) {
+  int growth = ((amount-1) * 100);
+  analogWrite(ledMains[branch], growth);
 }
--- Arduino/RecieveData/RecieveData.ino
+++ Arduino/RecieveData/RecieveData.ino
@@ -4,17 +4,20 @@
 
 #define NUM_BRANCHES 3
 
-int ledMains[NUM_BRANCHES] = { 11, 10, 9 };
+// Threshold values used to caluclate whether certain callees have been contacted enough
+// In Minutes per Month
+int expectedTalkTime[] = { 300, 50, 60 };
 
 void setup() {
   Serial.begin(9600);
   setupBranches();
-    Serial.println("<Arduino is ready>");
-  
 }
 
+// Call data recieved for each callee type
+// In Minutes per Month
 int dataIn[NUM_BRANCHES];
 
+// Serial Buffer data
 const byte numChars = 32;
 char receivedChars[numChars];
 char tempChars[numChars]; 
@@ -22,38 +25,51 @@ char tempChars[numChars];
 boolean newData = false;
 
 void loop() {
-  /* check if data has been sent from the computer: */
-  recvWithStartEndMarkers();
-   
+  // Look for data sent from Processing
+  recvWithStartEndMarkers('>', '<');
+
+  //Once all of a line from from Processing has been read in, deal with it
   if(newData) {
+    // Split data into ints stored in dataIn
     parseReadData();
+    
+    //Update each branch based on data received 
     for(int branch = 0; branch < NUM_BRANCHES; branch++) {
         int growthAmount = calcGrowth(branch, dataIn[branch]);
         grow(branch, growthAmount);
     }
-    newData = false;
+    newData = false; // Start looking for more data
   }
 }
 
-void recvWithStartEndMarkers() {
+/**
+ * Source: https://forum.arduino.cc/index.php?topic=396450
+ * Read a line of data in from the serial port, one character at a time. 
+ * Only read data between start and end markers to avoid unwanted behaviour.
+ * 
+ *  startMarker {char}  Character denoting the start of expected data
+ *  endMarker   {char}  Character denoting the end of expected data
+ *  
+ * Results are stored in 'receivedChars'.
+ */
+void recvWithStartEndMarkers(char startMarker, char endMarker) {
     static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = '>';
-    char endMarker = '<';
-    char rc;
+    static byte ndx = 0; //Index of the next char in the buffer string
+    char rc; //Char read in
  
     while (Serial.available() > 0 && newData == false) {
         rc = Serial.read();
 
+        // When recieving, accept any characters until the end marker
         if (recvInProgress == true) {
-            if (rc != endMarker) {
+            if (rc != endMarker) { //Reading the string
                 receivedChars[ndx] = rc;
                 ndx++;
                 if (ndx >= numChars) {
                     ndx = numChars - 1;
                 }
             }
-            else {
+            else { //Done receiving the string
                 receivedChars[ndx] = '\0'; // terminate the string
                 recvInProgress = false;
                 ndx = 0;
@@ -61,16 +77,29 @@ void recvWithStartEndMarkers() {
             }
         }
 
-        else if (rc == startMarker) {
+        else if (rc == startMarker) { //Start reading chars once the start char appears
             recvInProgress = true;
         }
     }
 }
 
+/**
+ * Parse data received over serial into something the program can use.
+ * 
+ * Expected String format: 3 numbers (a, b, c) separated by spaces where
+ *    a = Calls to/from Parents in Minutes per Month 
+ *    b = Calls to/from Grandparents in Minutes per Month 
+ *    c = Calls to/from Friends in Minutes per Month 
+ *    
+ * Results are stored in 'dataIn'.
+ */
 void parseReadData() {
+  // Copy receipt buffer into temp buffer to be safe
   strcpy(tempChars, receivedChars);
+
+  // c-string pointer used by strtok to hold results
+  char * strtokIndx;
   
-  char * strtokIndx; // this is used by strtok() as an index
   strtokIndx = strtok(tempChars," ");
   dataIn[PARENTS] = atoi(strtokIndx);
 
